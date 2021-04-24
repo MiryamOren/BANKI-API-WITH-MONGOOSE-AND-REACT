101deff5f9d05e0c255f35036ce9cc1f512b9589
Miryam Oren <miriam839@gmail.com>
2021-04-25 02:34:10 +0300

    commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const DB = process.env.DB || 'mongodb+srv://MiryamOren:FSciqc9dyj9-LJA@cluster0.r07fn.mongodb.net/BANK_API_DB?retryWrites=true&w=majority';
+const cors = require('cors');
+const DB = process.env.DB;
 mongoose.connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -12,7 +13,8 @@ const accountRouter = require('./routers/account');
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use(userRouter);
 app.use(accountRouter);
 
