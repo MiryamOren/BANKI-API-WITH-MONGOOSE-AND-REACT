c878fde0797f59524b1e72f6ad2e53ed321ca78b
Miryam Oren <miriam839@gmail.com>
2021-04-25 02:54:46 +0300

    commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const userRouter = require('./routers/user');
 const accountRouter = require('./routers/account');
 
 const app = express()
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
