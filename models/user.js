0dbb21ba8c089e91623e3d53dabc730e70cce5b1
Miryam Oren <miriam839@gmail.com>
2021-04-24 22:31:34 +0300

    commit

--- models/user.js
+++ models/user.js
@@ -5,10 +5,12 @@ const User = mongoose.model('user', {
   name: {
     type: String,
     require: true,
+    unique: true
   },
   email: {
     type: String,
     require: true,
+    unique: true,
     validate(str){
       if (!validator.isEmail(str)){
         throw new Error('Invalid email');
